ce222dd6bf33202164d0260d68c3bc0b6aca003f
hrivierefr <legsmower@gmail.com>
2018-11-20 19:03:16 +0100

    Tab ok et dropdown fonctionnel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,37 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery3
+//= require popper
+
+$(document).ready(function() {
+
+	//A l'ouverture
+	$('.category-todo').hide(); //Hide all tabs
+	$('.tab-link:first').addClass('active'); //Activate the first tab
+	$(".category-todo:first").fadeIn(); //Display first category
+	
+	//Au clic sur un tab
+	$('.tab-link').on('click',function(e) {
+		e.preventDefault(); //Prevent from the page to refresh or any link to open
+		$('.active').removeClass('active'); //Desactivate the previous tab
+ 		$(this).addClass('active'); //Activate the right tab
+		$('.category-todo').hide(); //Hide all tabs
+	 	$("#category-todo-"+e.target.id).fadeIn(); //Display the right category
+	})
+
+	//Au clic sur l'avatar
+	$('.avatar').on('click',function(e) {
+		e.preventDefault(); //Prevent from the page to refresh or any link to open
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+			$('.dropdown-menu').hide();
+		}
+		else {
+			$('.dropdown-menu').show();
+			$(this).addClass('active');
+		}
+	})
+
+
+})
