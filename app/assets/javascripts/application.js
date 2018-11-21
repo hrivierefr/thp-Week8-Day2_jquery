a9105573e1bdb9ac24a1a982fc4d3574aea33385
hrivierefr <legsmower@gmail.com>
2018-11-21 04:06:33 +0100

    Carousel ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -46,5 +46,90 @@ $(document).ready(function() {
 		}
 	})
 
+	//Au clic sur toggler
+	$('.navbar-toggler').on('click',function(e) {
+		e.preventDefault(); //Prevent from the page to refresh or any link to open
+		if ($('.collapse').hasClass('active')) {
+			$('.collapse').removeClass('active');
+			$('.collapse').hide();
+		}
+		else {
+			$('.collapse').show();
+			$('.collapse').addClass('active');
+		}
+	})
+
+
+    //Carousel-image
+    $('.carousel-image').hide(); //Hide all images
+
+	function indexUpdate(position) {
+		$('.active-image img').attr("src", 'https://png.pngtree.com/svg/20170826/carousel_dot_428068.png');
+		$('.active-image').removeClass('active-image');
+		$('.carousel-dot:eq('+position+')').addClass('active-image');
+		$('.active-image img').attr("src", 'https://static.thenounproject.com/png/120101-200.png');
+	}
+
+	function newActiveImage(position) {
+		indexUpdate(position);
+		$('.carousel-image').hide(); //Hide all images
+		$('#'+position+'.carousel-image').fadeIn();
+	}
+	
+	//A l'ouverture de la page
+    newActiveImage(0);
+
+    //Clic sur un dot
+	$('.carousel-dot').click(function(e) {
+		e.preventDefault(); //Prevent from the page to refresh or any link to open
+    	newActiveImage(e.currentTarget.id);
+	})
+
+	const imagesCount = Number($('.carousel-image:last').attr('id'));
+
+	//Clic sur previous
+	$('#carousel-previous').click(function(e) {
+		const activePosition = Number($('.active-image').attr('id'));
+
+		e.preventDefault(); //Prevent from the page to refresh or any link to open
+
+		if (activePosition === 0 ) {
+	    	newActiveImage(imagesCount);
+		}
+		else {
+			newActiveImage(activePosition-1);
+		}
+	})
+
+	//Clic sur next
+	$('#carousel-next').click(function(e) {
+		const activePosition = Number($('.active-image').attr('id'));
+
+		e.preventDefault(); //Prevent from the page to refresh or any link to open
+
+		if (activePosition === imagesCount ) {
+	    	newActiveImage(0);
+		}
+		else {
+			newActiveImage(activePosition+1);
+		}
+	})
+
+	//Modale
+/*   $("#signup-link").on('click',function(e) {
+    	e.preventDefault();
+      	$('#signup-modal').modal('show');
+	});
+    
+    $("#login-link").on('click',function(e) {
+    	e.preventDefault();
+      	$('#login-modal').modal('show');
+	});
+    
+    $("div:not('.modal')").on('click',function(e) {
+    	e.preventDefault();
+      	$('.modal').modal('hide');
+    });
+*/
 
 })
